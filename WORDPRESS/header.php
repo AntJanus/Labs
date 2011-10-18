fe6fe0fd7b35590d6132852cf61418462571a784
Antonin Januska <antonin@antjanus.com>
2011-10-17 22:25:50 -0600

    First Snippets

---
+++ WORDPRESS/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head profile="http://gmpg.org/xfn/11">
+
+	<title><?php wp_title(); ?></title>
+
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />	
+	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats please -->
+
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<meta name="viewport" content="width=device-width" />
+	<?php wp_get_archives('type=monthly&format=link'); ?>
+	<?php //comments_popup_script(); // off by default ?>
+	<?php wp_head(); ?>
+     
+     
+	<?php $templateUri = get_template_directory_uri(); ?>
+     
+     
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
---
+++ WORDPRESS/microformattedLoop.php
@@ -0,0 +1,19 @@
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <article class="post hentry hnews"><!-- START OF POST -->
+          
+<h1 class="entry-title url"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+<span class="meta"><time class="updated" datetime="<?php
+$postDate = get_the_date('c');
+$postDate2 = get_the_date('d.m.Y');
+echo $postDate ?>" pubdate>
+<?php echo $postDate2; ?></time> | <span class="byline vcard"><span class="fn author"><?php the_author();?></span> | <?php the_category(''); ?></span> | <span class"org"><?php wp_title(); ?></span> 
+        <div class="postContent entry-content">
+     <?php the_content(); ?>
+     
+     
+
+     <p class="postmetadata">Posted in <?php the_category(', '); ?> | Tags: <?php the_tags(); ?></p>
+     
+     </div>
+                </article><!-- END OF POST -->
+<?php endwhile; endif;?>
