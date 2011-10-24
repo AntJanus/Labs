bc6b164efc18249102b2d2640f5b8f0fd62265f8
Antonin Januska <Antonin@antjanus.com>
2011-10-24 15:21:39 -0500

    Major Microformatting Changes

--- WORDPRESS/microformattedLoop.php
+++ WORDPRESS/microformattedLoop.php
@@ -2,11 +2,11 @@
                 <article class="post hentry hnews"><!-- START OF POST -->
           
 <h1 class="entry-title url"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-<span class="meta"><time class="updated" datetime="<?php
+<span class="meta  vcard"><time class="updated" datetime="<?php
 $postDate = get_the_date('c');
 $postDate2 = get_the_date('d.m.Y');
 echo $postDate ?>" pubdate>
-<?php echo $postDate2; ?></time> | <span class="byline vcard"><span class="fn author"><?php the_author();?></span> | <?php the_category(''); ?></span> | <span class="org"><?php bloginfo('name'); ?></span></span> 
+<?php echo $postDate2; ?></time> | <span class="byline fn author"><?php the_author();?></span> | <?php the_category(''); ?> | <span class="source-org copyright"><?php bloginfo('name'); ?></span></span></span> 
    <div class="postContent entry-content">
      <?php the_content(); ?>
      
