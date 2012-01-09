212fe5e2956c98132d163ce297aec65fe5ff7195
Antonin Januska <antonin@antjanus.com>
2012-01-08 23:50:53 -0600

    Vertical Rhythm Tool Added

--- CSS/readme.md
+++ CSS/readme.md
@@ -3,3 +3,4 @@ Basic CSS components and snippets
 Demo Locations:
 highlighter.css - http://jsfiddle.net/antjanus/T9sCJ/
 floatingImageButton.html - http://jsfiddle.net/antjanus/P52Pk/
+verticalrhythmtool.html - http://jsfiddle.net/antjanus/rjUnY/
--- WORDPRESS/DChead.php
+++ WORDPRESS/DChead.php
@@ -1,44 +1,40 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
+<title>
+<?php wp_title(); ?>
+</title>
+<!-- FAVICON -->
+<link rel="shortcut icon" href="favicon.ico" />
+<meta name="generator" content="WordPress" />
+<meta name="title" content="<?php wp_title(''); ?>" />
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
+<!-- leave this for stats please -->
 
-	<title><?php wp_title(); ?></title>
-		<!-- FAVICON -->
-		<link rel="shortcut icon" href="favicon.ico" /><meta name="generator" content="WordPress" /> 
-		<meta name="title" content="<?php wp_title(''); ?>" />
-		
-		
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />	
-	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats please -->
-
-	<link rel="stylesheet" type="text/css" media="all" href="<?php get_stylesheet_directory_uri(); ?>" />
-	
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php get_stylesheet_directory_uri(); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <meta name="viewport" content="width=device-width" />
-	<?php wp_get_archives('type=monthly&format=link'); ?>
-	<?php //comments_popup_script(); // off by default ?>
-	<?php wp_head(); ?>
-     
-     
-	<?php $templateUri = get_template_directory_uri(); ?>
-     
-     
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-    <!--[if lt IE 9]>
+<?php wp_get_archives('type=monthly&format=link'); ?>
+<?php //comments_popup_script(); // off by default ?>
+<?php wp_head(); ?>
+<?php $templateUri = get_template_directory_uri(); ?>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<!--[if lt IE 9]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-    
-    <!-- BELOW IS A SAMPLE DUBLIN CORE META DATA HEADER -->
-   <link rel="schema.DC" href="http://purl.org/dc/elements/1.1/">
-	<meta name="DC.title" content="<?php wp_title(''); ?>">
-	<meta name="DC.description" content="<?php if ( is_single() ) {
+
+<!-- BELOW IS A SAMPLE DUBLIN CORE META DATA HEADER -->
+<link rel="schema.DC" href="http://purl.org/dc/elements/1.1/">
+<meta name="DC.title" content="<?php wp_title(''); ?>">
+<meta name="DC.description" content="<?php if ( is_single() ) {
         single_post_title('', true); 
     } else {
         bloginfo('name'); echo " - "; bloginfo('description');
     }
     ?>">
-	<meta name="DC.language" scheme="ISO639-1" content="en">
-    <meta name="DC.publisher" content="<?php bloginfo('name');?>">
-    <!-- END DUBLIN CORE -->
+<meta name="DC.language" scheme="ISO639-1" content="en">
+<meta name="DC.publisher" content="<?php bloginfo('name');?>">
+<!-- END DUBLIN CORE -->
 </head>
 <body <?php body_class();?>>
